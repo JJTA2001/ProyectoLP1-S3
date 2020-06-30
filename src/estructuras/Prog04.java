9c1ac587ad89fdc326b88542979f4456615348a6
Joseph Tamayo <supermister44@gmail.com>
2020-06-30 16:32:09 -0500

    Prog04 - Listo

---
+++ src/estructuras/Prog04.java
@@ -0,0 +1,17 @@
+
+package estructuras;
+
+
+
+public class Prog04 {
+    public static void main(String[] args) {    
+        int numero = 1;
+        String mensaje = " Cristo viene pronto - Prepárate";
+        while(numero <=10){
+            System.out.println(mensaje);
+            numero++;
+        }
+        
+        
+    }
+}
