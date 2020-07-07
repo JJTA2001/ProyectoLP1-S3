f24d832ede6465eed32bf6130d2c8e60172d8e6d
Joseph Tamayo <supermister44@gmail.com>
2020-07-07 14:13:44 -0500

    Prog 007 - Metodo Vacio

---
+++ src/modularidad/Prog007.java
@@ -0,0 +1,22 @@
+
+package modularidad;
+// Autor: Joseph Tamayo
+
+public class Prog007 {
+    //Metodo vacio...
+    //Vacio porque dentro del parentesis esta vacio (no tiene parametros)...
+    public static void saludo(){
+            System.out.println("Bienvenido al sistema :D");
+    }
+    public static void mensaje(){
+        System.out.println("Entodo tiempo ama el amigo ...Prov. 17:17");
+    }
+    public static void despedida(){
+            System.out.println("Gracias por usar este Programa");
+    }
+    public static void main(String[] args) {
+        saludo();
+        mensaje();
+        despedida();
+    }
+}
